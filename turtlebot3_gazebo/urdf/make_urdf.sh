2b91c8d21cf5cce2c5924993537530581e61d5cd
Sergei <msghub@hotmail.com>
2023-02-02 17:02:14 -0600

    Removed all Create xacro files, manually edited waffle URDF and SDF to Create specs

--- turtlebot3_gazebo/urdf/make_urdf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#Note: do "colcon build" in ~/turtlebot_create_ws first!
-
-source ~/turtlebot_create_ws/install/setup.bash
-xacro turtlebot3_create_1.urdf.xacro > turtlebot3_create_1.urdf
-
